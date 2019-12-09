e9db816d9cbd5daa6907cd9e98ff85f15391cbca
David <41311088+biagiola@users.noreply.github.com>
2019-12-09 13:38:02 -0300

    useState hook added

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,25 @@
-import React from 'react'; //imr
+import React, { useState } from 'react'; //imr
+import uuid from 'uuid/v1';
 
 const SongList = () => { //sfc
+
+  const[songs, setSongs] = useState([
+    { id: 1, band: 'Tame Impala', title: 'It might be time', year: 2019},
+    { id: 2, band: 'DMAS', title: 'Silver', year: 2019}
+  ]);
+
+  const addSong = () => {
+    setSongs([...songs, { band: 'Oasis', title: 'new song added', id: uuid() }]);
+  }
+
   return (
-    <div classNmae="song-list">
+    <div className="song-list">
       <ul>
-        <li>DMA'S - Silver</li>
-        <li>Tame Impala - It my be time</li>
+        { songs.map(song => {
+          return ( <li key={song.id}> {song.band} - {song.title}</li> )
+        })}
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
   );
 }
